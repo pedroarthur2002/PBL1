25343b343297cf89edeee0d7cfa3320117a7f7a7
Pedro Arthur <panrc100@gmail.com>
2023-08-14 01:49:27 -0300

    declaração das variáveis

---
+++ Projeto PBL 1.py
@@ -0,0 +1,15 @@
+#VARIÁVEIS
+posto_1= input('Digite o nome do primeiro posto: ')
+gasolina_1= float(input('Digite o valor da gasolina no primeiro posto: '))
+etanol_1= float(input('Digite o valor do etanol no primeiro posto: '))
+diesel_1= float(input('Digite o preço no diesel no primeiro posto: '))
+
+posto_2= input('Digite o nome do segundo posto: ')
+gasolina_2= float(input('Digite o valor da gasolina no segundo posto: '))
+etanol_2= float(input('Digite o valor do etanol no segundo posto: '))
+diesel_2= float(input('Digite o preço no diesel no segundo posto: '))
+
+posto_3= input('Digite o nome do terceiro posto: ')
+gasolina_3= float(input('Digite o valor da gasolina no terceiro posto: '))
+etanol_3= float(input('Digite o valor do etanol no terceiro posto: '))
+diesel_3= float(input('Digite o preço no diesel no terceiro posto: '))
